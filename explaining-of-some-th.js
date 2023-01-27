f6ec72f21eab7345e39fca7de3af0f7e160c14b0
Aziz Sattorov <108568027+thtflx@users.noreply.github.com>
2023-01-28 00:47:43 +0300

    Add new promises

--- explaining-of-some-th.js
+++ explaining-of-some-th.js
@@ -20,34 +20,59 @@
 
 
 //* настоящие promise'ы.
-const somePromise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        console.log(`Preparing data...`);
-        const backendData = {
-            server: 'aws',
-            port: 2000,
-            status: 'working'
-        }
-        resolve(backendData);
-    }, 2000);
-});
+// const somePromise = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         console.log(`Preparing data...`);
+//         const backendData = {
+//             server: 'aws',
+//             port: 2000,
+//             status: 'working'
+//         }
+//         resolve(backendData);
+//     }, 2000);
+// });
 
 
-somePromise
-    .then((data) => {
-        return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                data.modified = true;
-                resolve(data);
-            }, 2000);
-        });
+// somePromise
+//     .then((data) => {
+//         return new Promise((resolve, reject) => {
+//             setTimeout(() => {
+//                 data.modified = true;
+//                 resolve(data);
+//             }, 2000);
+//         });
+//     })
+//     .then((clientData) => {
+//         clientData.fromPromise = true;
+//         return clientData;
+//     })
+//     .then((data) => {
+//         console.log(`Modified`, data);
+//     })
+//     .catch((err) => console.log(`Error: `, err))
+//     .finally(() => console.log(`Finally`))
+
+
+//* трюк с ракетой.
+const sleep = (ms) => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve();
+        }, ms);
     })
-    .then((clientData) => {
-        clientData.fromPromise = true;
-        return clientData;
-    })
-    .then((data) => {
-        console.log(`Modified`, data);
-    })
-    .catch((err) => console.log(`Error: `, err))
-    .finally(() => console.log(`Finally`))
+}
+
+sleep(1000).then(() => console.log(`3....`))
+sleep(2000).then(() => console.log(`2...`))
+sleep(3000).then(() => console.log(`1..`))
+sleep(4000).then(() => console.log(`Rocket has been flown.`))
+
+
+//* Еще 2 promise'a.
+// Promise.all([sleep(2000), sleep(5000)]).then(() => {
+//     console.log(`All promises`);
+// })
+
+Promise.race([sleep(2000), sleep(5000)]).then(() => {
+    console.log(`Race promises`);
+})
