6fada42e2fafe29fea12dd54d60e5bfa3bb046c0
Aziz Sattorov <108568027+thtflx@users.noreply.github.com>
2023-01-27 23:57:42 +0300

    Add main themes to other repo

---
+++ explaining-of-some-th.js
@@ -0,0 +1,17 @@
+// последовательная асинхронность с помощью CALLBACK.
+console.log(`Request data...`);
+
+setTimeout(() => {
+    console.log(`Preparing data...`);
+
+    const backendData = {
+        server: 'aws',
+        port: 2000,
+        status: 'working'
+    }
+
+    setTimeout(() => {
+        backendData.modified = true;
+        console.log(`Data received`, backendData);
+    }, 2000);
+}, 2000);
