a6c64889cb068ab0f3524c55ea42fabb465d57a1
Aziz Sattorov <108568027+thtflx@users.noreply.github.com>
2023-01-28 00:31:02 +0300

    upd

--- explaining-of-some-th.js
+++ explaining-of-some-th.js
@@ -20,7 +20,7 @@
 
 
 //* настоящие promise'ы.
-const p = new Promise((resolve, reject) => {
+const somePromise = new Promise((resolve, reject) => {
     setTimeout(() => {
         console.log(`Preparing data...`);
         const backendData = {
@@ -33,17 +33,21 @@ const p = new Promise((resolve, reject) => {
 });
 
 
-p.then((data) => {
-    const p2 = new Promise((resolve, reject) => {
-        setTimeout(() => {
-            data.modified = true;
-            resolve(data);
-        }, 2000);
-    });
-
-    p2.then((clientData) => {
-        console.log(`Data received`, clientData);
-    });
-
-
-})
+somePromise
+    .then((data) => {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                data.modified = true;
+                resolve(data);
+            }, 2000);
+        });
+    })
+    .then((clientData) => {
+        clientData.fromPromise = true;
+        return clientData;
+    })
+    .then((data) => {
+        console.log(`Modified`, data);
+    })
+    .catch((err) => console.log(`Error: `, err))
+    .finally(() => console.log(`Finally`))
