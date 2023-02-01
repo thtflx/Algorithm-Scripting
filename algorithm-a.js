cfa5518b22def8e6a98f899a3e60795c3f3fc596
Aziz Sattorov <108568027+thtflx@users.noreply.github.com>
2023-02-01 16:07:43 +0300

    upd changes

--- algorithm-a.js
+++ algorithm-a.js
@@ -110,32 +110,6 @@ function largestOfFour(arr) {
     return answer;
 }
 
-// mytry
-function findLargestNumber(arr) {
-    let answer = [];
-
-    for (let i = 0; i < arr.length; i++) {
-        let highest = arr[i][0];
-
-        for (let j = 0; j < arr[i].length; j++) {
-            if (arr[i][j] > highest) {
-                highest = arr[i][j];
-            }
-        }
-        answer.push(highest);
-    }
-    return answer;
-}
-
-console.log(largestOfFour([
-    [4, 5, 1, 3],
-    [13, 27, 18, 26],
-    [32, 35, 37, 39],
-    [1000, 1001, 857, 1],
-    [2223, 556, 65, 34]
-]));
-
-
 
 
 
